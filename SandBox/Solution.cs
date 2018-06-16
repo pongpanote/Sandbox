cd3e383786ccab01703b4a201a29f434a612d7e1
Pongpanote.Gongsook <Pongpanote.Gongsook@schange.com>
2018-06-16 22:07:28 +0200

    getOneBits

--- SandBox/Solution.cs
+++ SandBox/Solution.cs
@@ -15,82 +15,24 @@ using System.Numerics;
 
 class Solution
 {
-
-    // Complete the timeInWords function below.
-    private static readonly string[] m_Hours = {
-        "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-        "eleven", "twelve"};
-
-    private static readonly string[] m_Minutes = {
-        "", "one", "two", "three", "four", "five", "six","seven", "eight", "nine", "ten",
-        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
-        "twenty one", "twenty two", "twenty three", "twenty four", "twenty five",
-        "twenty six", "twenty seven", "twenty eight", "twenty nine" };
-
-    static string TimeInWords(int h, int m)
+    static int[] getOneBits(int n)
     {
-        if (m == 0)
+        var binaryString = Convert.ToString(n, 2);
+        var sumOfOne = binaryString.Count(x=>x=='1');
+        var result = new List<int>();
+
+        result.Add(sumOfOne);
+
+        for (var i = 0; i < binaryString.Length; i++)
         {
-            return OClock(h);
+            if (binaryString[i] == '1')
+            {
+                result.Add(i + 1);
+            }
         }
 
-        if (m <= 30)
-        {
-            return PastHour(h, m);
-        }
-
-        return ToHour(h, m);
+        return result.ToArray();
     }
 
-    private static string OClock(int h)
-    {
-        return string.Concat(m_Hours[h], " o' clock");
-    }
-
-    private static string PastHour(int h, int m)
-    {
-        if (m == 1)
-        {
-            return string.Concat("one minute past ", m_Hours[h]);
-        }
-        if (m == 15)
-        {
-            return string.Concat("quarter past ", m_Hours[h]);
-        }
-        if (m == 30)
-        {
-            return string.Concat("half past ", m_Hours[h]);
-        }
-        return string.Concat(m_Minutes[m], " minutes past ", m_Hours[h]);
-    }
-
-    private static string ToHour(int h, int m)
-    {
-        h = h == 12 ? 1 : h + 1;
-        m = 60 - m;
-
-        if (m == 15)
-        {
-            return string.Concat("quarter to ", m_Hours[h]);
-        }
-        return string.Concat(m_Minutes[m], " minutes to ", m_Hours[h]);
-    }
-
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int h = Convert.ToInt32(Console.ReadLine());
-
-        int m = Convert.ToInt32(Console.ReadLine());
-
-        string result = TimeInWords(h, m);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
 }
 
