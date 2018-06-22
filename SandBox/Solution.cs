b96a99d49972b7d0e410e9d8c03fa1d050db2d2f
Pongpanote.Gongsook <Pongpanote.Gongsook@schange.com>
2018-06-22 12:55:47 +0200

    2 characters and climming leaderboard

--- SandBox/Solution.cs
+++ SandBox/Solution.cs
@@ -1,38 +1,148 @@
-﻿using System.CodeDom.Compiler;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
 using System.Text;
-using System;
-using System.Numerics;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-class Solution
+public class Solution
 {
-    static int[] getOneBits(int n)
+    static int[] climbingLeaderboard(int[] scores, int[] alice)
     {
-        var binaryString = Convert.ToString(n, 2);
-        var sumOfOne = binaryString.Count(x=>x=='1');
-        var result = new List<int>();
+        var scoreRank = scores.Distinct().ToArray();
+        var scoreRankReversed = scoreRank.Reverse().ToArray();
 
-        result.Add(sumOfOne);
+        return alice.Select(score => GetScorePosition(score, scoreRankReversed)).ToArray();
+    }
 
-        for (var i = 0; i < binaryString.Length; i++)
+    static int GetScorePosition(int score, int[] scoreRankReversed)
+    {
+        var rank = Array.BinarySearch(scoreRankReversed, score);
+        if (rank < 0)
         {
-            if (binaryString[i] == '1')
+            rank = ~rank;
+            if (rank == 0)
             {
-                result.Add(i + 1);
+                return scoreRankReversed.Length + 1;
+            }
+
+            if (rank == scoreRankReversed.Length)
+            {
+                return 1;
             }
         }
 
-        return result.ToArray();
+        if (score == scoreRankReversed[rank])
+        {
+            return scoreRankReversed.Length - rank;
+        }
+
+        return scoreRankReversed.Length - rank + 1;
     }
 
-}
 
+    static int twoCharaters(string s)
+    {
+        if (s.Length < 2)
+        {
+            return 0;
+        }
+
+        var tuplesHashSet = GetPermutation(new HashSet<char>(s.ToCharArray()));
+
+        var trimming = Trimming(s, tuplesHashSet);
+
+        var distinctAlternate = DistinctlyAlternate(trimming);
+
+        return distinctAlternate.Any() ? distinctAlternate.First().Length : 0;
+    }
+
+    private static IOrderedEnumerable<string> DistinctlyAlternate(HashSet<IEnumerable<char>> trimming)
+    {
+        var result = new HashSet<string>();
+        foreach (var item in trimming)
+        {
+            var charArray = item.ToArray();
+            if (charArray.Length == 1)
+            {
+                result.Add(string.Concat(charArray));
+                continue;
+            }
+
+            var distinct = true;
+            for (var i = 0; i < charArray.Length - 1; i++)
+            {
+                if (charArray[i] == charArray[i + 1])
+                {
+                    distinct = false;
+                    break;
+                }
+            }
+
+            if (distinct)
+            {
+                result.Add(string.Concat(charArray));
+            }
+        }
+
+        return result.ToList().OrderByDescending(str => str.Length);
+    }
+
+    private static HashSet<IEnumerable<char>> Trimming(string s, HashSet<Tuple<char, char>> tuplesHashSet)
+    {
+        var trimming = new HashSet<IEnumerable<char>>();
+
+        foreach (var tuple in tuplesHashSet)
+        {
+            trimming.Add(s.ToCharArray().Where(ch => ch == tuple.Item1 || ch == tuple.Item2));
+        }
+
+        return trimming;
+    }
+
+    private static HashSet<Tuple<char, char>> GetPermutation(HashSet<char> charHashSet)
+    {
+        var permutation = new HashSet<Tuple<char, char>>();
+
+        while (charHashSet.Count >= 2)
+        {
+            var ch = charHashSet.First();
+            charHashSet.Remove(ch);
+            foreach (var c in charHashSet)
+            {
+                permutation.Add(new Tuple<char, char>(ch, c));
+            }
+        }
+
+        return permutation;
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        var result1 = climbingLeaderboard(new[]
+        {
+            100, 100, 50, 40, 40, 20, 10
+        }, new[]
+        {
+            5, 25, 50, 120
+        });
+
+        Console.WriteLine(string.Join("\n", result1));
+
+
+        var l = Convert.ToInt32(Console.ReadLine());
+
+        var s = Console.ReadLine();
+
+        var result = twoCharaters(s);
+
+        Console.ReadLine();
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
---
+++ SandBox/Solution1.cs
@@ -0,0 +1,140 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Numerics;
+
+class Solution1
+{
+
+    // Complete the biggerIsGreater function below.
+    private static string BiggerIsGreater(string w)
+    {
+        var charValue = w.ToCharArray().Select(ch => Convert.ToInt32(ch)).ToArray();
+
+        for (var i = charValue.Length - 1; i >= 0; i--)
+        {
+            int[] reOrdered;
+            if (i > 0)
+            {
+                reOrdered = ReorderFromIndex(charValue, i, OrderByDescending);
+                if (!IsGreater(reOrdered, charValue))
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                reOrdered = SwapTheNextGreaterChar(charValue);
+            }
+
+            var result = ReorderFromIndex(reOrdered, i + 1, OrderByAscending);
+
+            if (IsGreater(result, charValue))
+            {
+                return ToString(result);
+            }
+        }
+        
+        return "no answer";
+    }
+
+    private static int[] SwapTheNextGreaterChar(int[] charValue)
+    {
+        var firstChar = charValue[0];
+
+        var charValueCopy = charValue.ToArray();
+
+        for (var i = charValue.Length - 1; i > 0; i--)
+        {
+            if (charValue[i] <= firstChar)
+            {
+                continue;
+            }
+
+            charValueCopy[0] = charValueCopy[i];
+            charValueCopy[i] = firstChar;
+
+            return charValueCopy; 
+        }
+
+        return charValueCopy;
+    }
+
+
+    private static string ToString(int[] charArray)
+    {
+        var strArray = charArray.Select(ch => Convert.ToChar(ch));
+
+        return string.Concat(strArray);
+    }
+
+    private static bool IsGreater(int[] reOrdered, int[] charValue)
+    {
+        return reOrdered.Where((t, i) => t > charValue[i]).Any();
+    }
+
+    private static int[] ReorderFromIndex(int[] charValue, int fromThisIndex, Func<IEnumerable<int>, IEnumerable<int>> orderFunction)
+    {
+        var left = charValue.Take(charValue.Length - (charValue.Length - fromThisIndex)).ToList();
+        var right = charValue.Skip(fromThisIndex);
+
+        right = orderFunction(right);
+        left.AddRange(right);
+
+        return left.ToArray();
+    }
+
+    private static IEnumerable<int> OrderByDescending(IEnumerable<int> right)
+    {
+        return right.OrderByDescending(ch => ch);
+    }
+
+    private static IEnumerable<int> OrderByAscending(IEnumerable<int> right)
+    {
+        return right.OrderBy(ch => ch);
+    }
+
+
+    public static void SMain(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        var t = Convert.ToInt32(Console.ReadLine());
+
+        for (var tItr = 0; tItr < t; tItr++)
+        {
+            var w = Console.ReadLine();
+
+            var result = BiggerIsGreater(w);
+
+            Console.WriteLine(result);
+
+            //textWriter.WriteLine(result);
+        }
+
+        Console.ReadLine();
+
+        //textWriter.Flush();
+        //textWriter.Close();
+
+
+        //5
+        //ab      ba
+        //bb
+        //hefg    hegf
+        //dhck    dhkc
+        //dkhc    hcdk
+
+    }
+}
+
